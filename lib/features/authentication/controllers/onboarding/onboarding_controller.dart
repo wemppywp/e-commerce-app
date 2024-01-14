944700f40bd8f7b49e9c4f6dc44207b15b376a27
Wemppy Wp <wemppywp@gmail.com>
2024-01-14 16:00:51 +0700

    update depedencies is not used

--- lib/features/authentication/controllers/onboarding/onboarding_controller.dart
+++ lib/features/authentication/controllers/onboarding/onboarding_controller.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get_storage/get_storage.dart';
 import 'package:w_store/features/authentication/screens/login/login_screen.dart';
 
 class OnBoardingController extends GetxController{
--- lib/features/authentication/screens/login/login_screen.dart
+++ lib/features/authentication/screens/login/login_screen.dart
@@ -1,13 +1,8 @@
 import 'package:flutter/material.dart';
 
-class LoginScreen extends StatefulWidget {
+class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
-  @override
-  State<LoginScreen> createState() => _LoginScreenState();
-}
-
-class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
